a8c8db99ff738f484bd5fc463518e1b8597611a8
Camden Lopez <camden.lopez@gmail.com>
2015-10-19 23:07:14 -0700

    Scan twice and output all relevant columns with their suffixes

--- filter.rb
+++ filter.rb
@@ -15,32 +15,64 @@ output = ARGV[2]
 
 require query
 
-CSV.open(output, "w") do |out|
-  out << $columns
-  CSV.foreach(input, headers: true) do |row|
-    includerow = true
-    for key in $query.keys
-      satisfies = false
-      for suffix in $suffixes
-        if !row[key + suffix].nil? && $query[key].include?(row[key + suffix])
-          satisfies = true
-        end
-      end
-      if !satisfies
-        includerow = false
-      end
-    end
-    if includerow
-      values = []
-      found = nil
-      for column in $columns
-        for suffix in $suffixes
-          if !row[column + suffix].nil?
-            found = row[column + suffix]
+def include?(row)
+  includerow = true
+  for column in $query.keys
+    satisfies = false
+    for suffix in $suffixes
+      unless row[column + suffix].nil? || row[column + suffix].empty?
+        for desired in $query[column]
+          if row[column + suffix].include?(desired)
+            satisfies = true
             break
           end
         end
-        values << found
+      end
+    end
+    unless satisfies
+      includerow = false
+      break
+    end
+  end
+  return includerow
+end
+
+include_column = {}
+for column in $columns
+  include_column[column] = {}
+  for suffix in $suffixes
+    include_column[column][suffix] = false
+  end
+end
+
+CSV.foreach(input, headers: true) do |row|
+  if include?(row)
+    for column in $columns
+      for suffix in $suffixes
+        unless row[column + suffix].nil? || row[column + suffix].empty?
+          include_column[column][suffix] = true
+        end
+      end
+    end
+  end
+end
+
+all_columns = []
+for column in include_column.keys
+  for suffix in include_column[column].keys
+    if include_column[column][suffix]
+      all_columns.push(column + suffix)
+    end
+  end
+end
+
+CSV.open(output, "w") do |out|
+  out << all_columns
+  CSV.foreach(input, headers: true) do |row|
+    if include?(row)
+      values = []
+      for column in all_columns
+        values.push(row[column])
       end
       out << values
     end
